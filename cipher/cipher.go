4cab035c782d1fbd78b4c9e5c6c4b11ad8099d7c
chenghongxi <422459409@qq.com>
2022-10-28 14:37:26 +0800

    required

--- cipher/cipher.go
+++ cipher/cipher.go
@@ -8,8 +8,11 @@ import (
 	"crypto/cipher"
 )
 
-var AES_KEY string
-var AES_IV string
+// Define “Config” struct,Save AES and KEY,IV
+type Config struct {
+	AES_KEY string
+	AES_IV  string
+}
 
 type NewCipher struct {
 }
@@ -18,29 +21,29 @@ func New() *NewCipher {
 	return &NewCipher{}
 }
 
-func (e *NewCipher) Encrypt(data []byte, AES_KEY string, AES_IV string) (string, error) {
-	block, err := aes.NewCipher([]byte(AES_KEY))
+func (e *NewCipher) Encrypt(data []byte, config Config) (string, error) {
+	block, err := aes.NewCipher([]byte(config.AES_KEY))
 	if err != nil {
 		return "", err
 	}
 	blockSize := block.BlockSize()
 	originData := pad(data, blockSize)
-	blockMode := cipher.NewCBCEncrypter(block, []byte(AES_IV))
+	blockMode := cipher.NewCBCEncrypter(block, []byte(config.AES_IV))
 	crypted := make([]byte, len(originData))
 	blockMode.CryptBlocks(crypted, originData)
 	return base64.StdEncoding.EncodeToString(crypted), nil
 }
 
-func (e *NewCipher) Decrypt(decryptText string, AES_KEY string, AES_IV string) ([]byte, error) {
+func (e *NewCipher) Decrypt(decryptText string, config Config) ([]byte, error) {
 	decodeData, err := base64.StdEncoding.DecodeString(decryptText)
 	if err != nil {
 		return nil, err
 	}
-	block, err := aes.NewCipher([]byte(AES_KEY))
+	block, err := aes.NewCipher([]byte(config.AES_KEY))
 	if err != nil {
 		return nil, err
 	}
-	blockMode := cipher.NewCBCDecrypter(block, []byte(AES_IV))
+	blockMode := cipher.NewCBCDecrypter(block, []byte(config.AES_IV))
 	originData := make([]byte, len(decodeData))
 	blockMode.CryptBlocks(originData, decodeData)
 	return unPad(originData), nil
--- examples/cipher/main.go
+++ examples/cipher/main.go
@@ -6,30 +6,26 @@ import (
 	"github.com/chenghongxi/tools/cipher"
 )
 
-type Ciphers struct {
-	key string
-	iv  string
-}
-
 func main() {
 
 	// 自定义变量b 类型为：byte数组
 	b := []byte("hello,world")
 
-	// 自定义AES_KEY AES_IV
-	AES_KEY := "KHGSI69YBWGS0TXB"
-	AES_IV := "3010201735544646"
+	NewConfig := cipher.Config{
+		AES_KEY: "KHGSI69YBWGS0TXB",
+		AES_IV:  "3010201735544646",
+	}
 
 	// 加密b
 	c := cipher.New()
-	encrypt, err := c.Encrypt(b, AES_KEY, AES_IV)
+	encrypt, err := c.Encrypt(b, NewConfig)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println("加密：", encrypt)
 
 	// 解密
-	decrypt, err := c.Decrypt(encrypt, AES_KEY, AES_IV)
+	decrypt, err := c.Decrypt(encrypt, NewConfig)
 	if err != nil {
 		fmt.Println(err)
 	}
